a97cd8c673f5aca532d491432166104631419323
swonny <wonny921@gmail.com>
2022-09-29 22:04:01 +0900

    22.09.29 3장

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Studying Node.js
 |완료|날짜|단원|인덱스|
 |:--:|:--:|:--:|:--:|
 |:heavy_check_mark:|09.27|01. 노드 시작하기|[1장](#01-노드-시작하기)|
-|:white_large_square:|09.27|04장|-|
-|:white_large_square:|09.28|05장|-|
+|:heavy_check_mark:|09.28|02장|[2장](#02-알아야-할-자바스크립트)|
+|:white_large_square:|09.29|03장|-|
 |:white_large_square:|09.29|06장|-|
 |:white_large_square:|09.30|07장|-|
 |:white_large_square:|10.01|08장|-|
@@ -19,9 +19,10 @@ Studying Node.js
 :white_large_square: Q. fork하여 자식 프로세스를 생성했던 것은 스레드인가? 프로세스인가?<br/>
 :white_large_square: Q. 별도의 웹 서버를 설치해야한다는 게 무슨 의미인가?<br/>
 
-## 02장
+## 02. 알아야 할 자바스크립트
+:white_large_square: Q. 콜백, 프로미스, async/await 개념
 
-## 03장
+## 03. 노드 기능 알아보기
 
 ## 04장
 
--- test.js
+++ test.js
@@ -1,12 +1,21 @@
-function first() {
-    second();
-    console.log('첫 번째')
-}
-function second() {
-    third();
-    console.log('두 번째')
-}
-function third() {
-    console.log('세 번째')
-}
-first()
+const os = require('os');
+
+console.log('------------운영체제 정보----------');
+console.log(os.arch());
+console.log(os.platform());
+console.log(os.type());
+console.log(os.uptime());
+console.log(os.hostname());
+console.log(os.release());
+
+console.log('------------경로----------');
+console.log(os.homedir());
+console.log(os.tmpdir());
+
+console.log('------------cpu 정보----------');
+console.log(os.cpus());
+console.log(os.cpus().length);
+
+console.log('------------메모리 정보----------');
+console.log(os.freemem());
+console.log(os.totalmem());
