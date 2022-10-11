fb963ef16b05d788585ec3fef3353b35abcbe0d0
swonny <wonny921@gmail.com>
2022-10-11 17:46:32 +0900

    docs: readme 3장 완료 반영

---
+++ 03_노드기능알아보기/노드_exports/querystring.js
@@ -0,0 +1,6 @@
+const crypto = require('crypto');
+
+const cipher = crypto.createCipheriv('aes-256-cbc', '열쇠');
+let result = cipher.update('암호화할 문장', 'utf8', 'base64');
+result += cipher.final('base64');
+console.log(result)
